43137fa0c804b7f836c82f187a9bf62892f5cb8a
Jens Pettersson <jens.pettersson@live.se>
2018-02-13 11:14:41 +0100

    Renamed and changed the tests to show that it seems to be the complex id reference objects causing the bug

--- test/SlowTests/Bugs/LoadInProjectionWithComplexIdObjects.cs
+++ test/SlowTests/Bugs/LoadInProjectionWithComplexIdObjects.cs
@@ -7,13 +7,13 @@ using Xunit;
 
 namespace SlowTests.Bugs
 {
-    public class LoadInProjectionWithIdConcatenation : RavenTestBase
+    public class LoadInProjectionWithComplexIdObjects : RavenTestBase
     {
-        public class RuleReference
+        public class IdReference
         {
             public string Id { get; }
 
-            public RuleReference(string id)
+            public IdReference(string id)
             {
                 Id = id;
             }
@@ -22,9 +22,12 @@ namespace SlowTests.Bugs
         public class Package
         {
             public string Id { get; set; }
+            
             public string ReferenceId { get; set; }
-            public RuleReference Reference { get; set; }
-            public string SomethingElseShortReference {get; set; }
+            public IdReference ComplexRuleReference { get; set; }
+
+            public string SomethingElseReference {get; set; }
+            public IdReference ComplexSomethingElseReference { get; set; }
         }
 
         public class Rule
@@ -40,7 +43,7 @@ namespace SlowTests.Bugs
         }
 
         [Fact]
-        public void Projection_with_load_on_complex_object_with_string_concatenation_last()
+        public void Projection_with_load_on_complex_object_id_last()
         {
             using (var store = GetDocumentStore())
             {
@@ -48,8 +51,8 @@ namespace SlowTests.Bugs
                 {
                     session.Store(new Package
                     {
-                        SomethingElseShortReference = "123-A",
-                        Reference = new RuleReference("rules/1")
+                        SomethingElseReference = "somethingElse/123-A",
+                        ComplexRuleReference = new IdReference("rules/1")
                     });
                     session.Store(new Rule
                     {
@@ -58,7 +61,7 @@ namespace SlowTests.Bugs
                     });
                     session.Store(new SomethingElse
                     {
-                        Id = "SomethingElse/123-A",
+                        Id = "somethingElse/123-A",
                         Name = "Something else name"
                     });
 
@@ -70,8 +73,8 @@ namespace SlowTests.Bugs
                     var query = session.Query<Package>();
 
                     var projection = from package in query
-                        let rule = RavenQuery.Load<Rule>(package.Reference.Id) //This Load works as the concatination load occurs after
-                        let somethingElse = RavenQuery.Load<SomethingElse>("SomethingElse/" + package.SomethingElseShortReference) 
+                        let somethingElse = RavenQuery.Load<SomethingElse>(package.SomethingElseReference) 
+                        let rule = RavenQuery.Load<Rule>(package.ComplexRuleReference.Id)
                         select new
                         {
                             PackageId = package.Id,
@@ -88,7 +91,7 @@ namespace SlowTests.Bugs
         }
 
         [Fact]
-        public void Projection_with_load_on_complex_object_with_string_concatenation_first()
+        public void Projection_with_load_on_complex_object_id_first()
         {
             using (var store = GetDocumentStore())
             {
@@ -96,8 +99,8 @@ namespace SlowTests.Bugs
                 {
                     session.Store(new Package
                     {
-                        SomethingElseShortReference = "123-A",
-                        Reference = new RuleReference("rules/1")
+                        SomethingElseReference = "somethingElse/123-A",
+                        ComplexRuleReference = new IdReference("rules/1")
                     });
                     session.Store(new Rule
                     {
@@ -106,7 +109,7 @@ namespace SlowTests.Bugs
                     });
                     session.Store(new SomethingElse
                     {
-                        Id = "SomethingElse/123-A",
+                        Id = "somethingElse/123-A",
                         Name = "Something else name"
                     });
 
@@ -118,8 +121,8 @@ namespace SlowTests.Bugs
                     var query = session.Query<Package>();
 
                     var projection = from package in query
-                        let somethingElse = RavenQuery.Load<SomethingElse>("SomethingElse/" + package.SomethingElseShortReference) 
-                        let rule = RavenQuery.Load<Rule>(package.Reference.Id) //This Load causes an exception if it occurs after string concatination load
+                        let rule = RavenQuery.Load<Rule>(package.ComplexRuleReference.Id)
+                        let somethingElse = RavenQuery.Load<SomethingElse>(package.SomethingElseReference) 
                         select new
                         {
                             PackageId = package.Id,
@@ -136,7 +139,7 @@ namespace SlowTests.Bugs
         }
 
         [Fact]
-        public void Projection_with_load_on_simple_string_with_string_concatenation_first()
+        public void Projection_with_load_using_only_complex_object_ids()
         {
             using (var store = GetDocumentStore())
             {
@@ -144,7 +147,56 @@ namespace SlowTests.Bugs
                 {
                     session.Store(new Package
                     {
-                        SomethingElseShortReference = "123-A",
+                        ComplexSomethingElseReference = new IdReference("somethingElse/123-A"),
+                        ComplexRuleReference = new IdReference("rules/1")
+                    });
+                    session.Store(new Rule
+                    {
+                        Id = "rules/1",
+                        Name = "Test rule"
+                    });
+                    session.Store(new SomethingElse
+                    {
+                        Id = "somethingElse/123-A",
+                        Name = "Something else name"
+                    });
+
+                    session.SaveChanges();
+                }
+
+                using (var session = store.OpenSession())
+                {
+                    var query = session.Query<Package>();
+
+                    var projection = from package in query
+                        let rule = RavenQuery.Load<Rule>(package.ComplexRuleReference.Id)
+                        let somethingElse = RavenQuery.Load<SomethingElse>(package.ComplexSomethingElseReference.Id) 
+                        select new
+                        {
+                            PackageId = package.Id,
+                            RuleName = rule.Name,
+                            SomethingElseName = somethingElse.Name
+                        };
+
+                    var result = projection.First();
+
+                    Assert.Equal("Test rule", result.RuleName);
+                    Assert.Equal("Something else name", result.SomethingElseName);
+                }
+            }
+        }
+
+        //This just illustrates that the order doesn't matter if you don't use complex objets as reference ids
+        [Fact]
+        public void Projection_with_load_using_only_simple_strings()
+        {
+            using (var store = GetDocumentStore())
+            {
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new Package
+                    {
+                        SomethingElseReference = "somethingElse/123-A",
                         ReferenceId = "rules/1"
                     });
                     session.Store(new Rule
@@ -154,7 +206,7 @@ namespace SlowTests.Bugs
                     });
                     session.Store(new SomethingElse
                     {
-                        Id = "SomethingElse/123-A",
+                        Id = "somethingElse/123-A",
                         Name = "Something else name"
                     });
 
@@ -165,9 +217,11 @@ namespace SlowTests.Bugs
                 {
                     var query = session.Query<Package>();
 
+                    //Same order as the first failing test but this works as it uses a simple string for the reference id load
                     var projection = from package in query
-                        let somethingElse = RavenQuery.Load<SomethingElse>("SomethingElse/" + package.SomethingElseShortReference) 
-                        let rule = RavenQuery.Load<Rule>(package.ReferenceId) //This Load works as the ReferenceId is a simple string
+                        let rule = RavenQuery.Load<Rule>(package.ReferenceId) 
+                        let somethingElse = RavenQuery.Load<SomethingElse>(package.SomethingElseReference) 
+                        
                         select new
                         {
                             PackageId = package.Id,
